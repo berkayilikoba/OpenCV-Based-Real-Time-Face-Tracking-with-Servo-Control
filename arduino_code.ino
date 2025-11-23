d7c00267e23fc11c3bbc9ec964420995c05e7cf0
Berkay Ilıkoba <ilikobaberkay@gmail.com>
2025-11-24 00:54:56 +0300

    upload

---
+++ arduino_code.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+Servo myServo;
+int angle = 0;
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(9);
+}
+
+void loop() {
+  if (Serial.available()) {
+    angle = Serial.parseInt();
+    angle = constrain(angle, 0, 180);
+    myServo.write(angle);
+  }
+}
